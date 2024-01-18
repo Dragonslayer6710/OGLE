f7ba6ca7e598e4f4cd63242cce9e50ab982db0db
Dragonslayer6710 <74316385+Dragonslayer6710@users.noreply.github.com>
2024-01-18 11:53:59 +0000

    Updated OGLE.h: Commented all includes

--- OpenGLEngine/src/OGLE.h
+++ OpenGLEngine/src/OGLE.h
@@ -2,39 +2,39 @@
 
 // For use by OpenGLEngine applications
 
-#include "OpenGLEngine/Core/Base.h"
-
-#include "OpenGLEngine/Core/Application.h"
-#include "OpenGLEngine/Core/Layer.h"
-#include "OpenGLEngine/Core/Log.h"
-#include "OpenGLEngine/Core/Assert.h"
-
-#include "OpenGLEngine/Core/Timestep.h"
-
-#include "OpenGLEngine/Core/Input.h"
-#include "OpenGLEngine/Core/KeyCodes.h"
-#include "OpenGLEngine/Core/MouseCodes.h"
-#include "OpenGLEngine/Renderer/OrthographicCameraController.h"
-
-#include "OpenGLEngine/ImGui/ImGuiLayer.h"
-
-#include "OpenGLEngine/Scene/Scene.h"
-#include "OpenGLEngine/Scene/Entity.h"
-#include "OpenGLEngine/Scene/ScriptableEntity.h"
-#include "OpenGLEngine/Scene/Components.h"
-
-#include "OpenGLEngine/Project/Project.h"
+// #include "OpenGLEngine/Core/Base.h"
+// 
+// #include "OpenGLEngine/Core/Application.h"
+// #include "OpenGLEngine/Core/Layer.h"
+// #include "OpenGLEngine/Core/Log.h"
+// #include "OpenGLEngine/Core/Assert.h"
+// 
+// #include "OpenGLEngine/Core/Timestep.h"
+// 
+// #include "OpenGLEngine/Core/Input.h"
+// #include "OpenGLEngine/Core/KeyCodes.h"
+// #include "OpenGLEngine/Core/MouseCodes.h"
+// #include "OpenGLEngine/Renderer/OrthographicCameraController.h"
+// 
+// #include "OpenGLEngine/ImGui/ImGuiLayer.h"
+// 
+// #include "OpenGLEngine/Scene/Scene.h"
+// #include "OpenGLEngine/Scene/Entity.h"
+// #include "OpenGLEngine/Scene/ScriptableEntity.h"
+// #include "OpenGLEngine/Scene/Components.h"
+// 
+// #include "OpenGLEngine/Project/Project.h"
 
 // ---Renderer------------------------
-#include "OpenGLEngine/Renderer/Renderer.h"
-#include "OpenGLEngine/Renderer/Renderer2D.h"
-#include "OpenGLEngine/Renderer/RenderCommand.h"
-
-#include "OpenGLEngine/Renderer/Buffer.h"
-#include "OpenGLEngine/Renderer/Shader.h"
-#include "OpenGLEngine/Renderer/Framebuffer.h"
-#include "OpenGLEngine/Renderer/Texture.h"
-#include "OpenGLEngine/Renderer/VertexArray.h"
-
-#include "OpenGLEngine/Renderer/OrthographicCamera.h"
+// #include "OpenGLEngine/Renderer/Renderer.h"
+// #include "OpenGLEngine/Renderer/Renderer2D.h"
+// #include "OpenGLEngine/Renderer/RenderCommand.h"
+// 
+// #include "OpenGLEngine/Renderer/Buffer.h"
+// #include "OpenGLEngine/Renderer/Shader.h"
+// #include "OpenGLEngine/Renderer/Framebuffer.h"
+// #include "OpenGLEngine/Renderer/Texture.h"
+// #include "OpenGLEngine/Renderer/VertexArray.h"
+// 
+// #include "OpenGLEngine/Renderer/OrthographicCamera.h"
 // -----------------------------------
